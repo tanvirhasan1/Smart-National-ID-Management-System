6c29f1ba8809e533bbd93d1ae1a8fa7496c1451e
Subho Dhar <subhodhar404@gmail.com>
2026-03-31 12:05:16 +0600

    add user profile update api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { getUserProfile } = require('../controllers/userController');
+const {
+  getUserProfile,
+  updateUserProfile
+} = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Get logged-in user profile
 router.get('/profile', protect, getUserProfile);
+
+// Update logged-in user profile
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
